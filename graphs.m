3f81d3de73b891cbb8b12ffb8e9c62877d8f0ff2
Eddie Richter <edwardrichter@email.arizona.edu>
2017-11-11 22:37:52 -0700

    Updated graph

--- graphs.m
+++ graphs.m
@@ -1,33 +1,54 @@
 close all; clear; 
 
 % Part 1 - Pie Chart
+figure;
 [numData,textData] = xlsread('part1.csv');
-pie(numData, textData);
+pie(numData);
 title('Graph 1 - AS Classification');
-figure
+legend(textData)
 
 % Part 2 - Histogram
+% Changed to get correct format and normalize
+figure;
 [numDataHistogram] = xlsread('part2.csv');
-edges = [1 2 5 100 200 1000 Inf];
-histogram(numDataHistogram(:,2), edges);
-title('Graph 2 - Node Degree Histogram');
 [a,b] = size(numDataHistogram);
-ylim([0 a])
-xlim([1 1200]);
-figure; 
+edges = [1 2 5 100 200 1000 Inf];
+binDescription = {'1','2-5','5-100','100-200','200-1000','>1000'};  
+bin(1) = sum(numDataHistogram(:,2)  == 1);
+bin(2) = sum(numDataHistogram(:,2) > 2 & numDataHistogram(:,2) < 5);
+bin(3) = sum(numDataHistogram(:,2) > 5 & numDataHistogram(:,2) < 100);
+bin(4) = sum(numDataHistogram(:,2) > 100 & numDataHistogram(:,2) < 200);
+bin(5) = sum(numDataHistogram(:,2) > 200 & numDataHistogram(:,2) < 1000);
+bin(6) = sum(numDataHistogram(:,2) > 1000);
+h = bar(bin/a, 'hist');
+set(gca, 'xticklabel', binDescription);
+set(h,'FaceColor', [0.5843 0.8157 0.9882],'EdgeColor','black');
+%c = histc(numDataHistogram(:,2), 'BinEdges'edges);
+%bar(edges, c)
+%histogram(numDataHistogram(:,2), 6, 'Normalization', 'count', 'BinEdges', edges);
+%histogram('BinEdges', edges, 'BinCounts', bin/a);
+title('Graph 2 - Node Degree Histogram');
+xlabel('AS node degree')
+ylabel('Count normalized to number of ASes')
+%ylim([0 a])
+%xlim([1 1200]);
 
 % Part 3 - Histogram
+figure;
 [numDataHistogram] = xlsread('part3.csv');
 edges = 2.^(0:20-1);
 edges = [0, edges, Inf];
 histogram(numDataHistogram, edges);
 title('Graph 3 - IP Space Histogram');
+xlabel('IP Space')
+ylabel('Count')
 %ylim([0 2^32])
 xlim([0 10000]);
-figure;
 
 % Part 4 - Pie Chart
+figure;
 [numData,textData] = xlsread('part4.csv');
-pie(numData, textData);
-title('Graph 4` - AS Classification');
+pie(numData);
+title('Graph 4 - AS Classification');
+legend(textData)
 
